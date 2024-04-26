a5f96f1578c025c7003e660631911741383dff41
Oussama <remlioussamapro@gmail.com>
2024-04-26 18:35:31 +0100

    myresume

--- MyPortfolio/src/components/header/Header.jsx
+++ MyPortfolio/src/components/header/Header.jsx
@@ -10,14 +10,13 @@ function Header() {
         <h1>Oussama Remli</h1>
         <h5 className="text-light">4th year student in Computer Engineering at the National School of Applied Sciences</h5><br></br>
         <div className='cta'>
-          {/*<a href={Resume} download className='btn'>Download My Resume</a>*/}
-          <a href="mailto:remlioussama@gmail.com" className='btn btn-primary'>Contact me</a>
+          <a href="MyPortfolio/static/Resume-An.pdf" className='btn btn-primary' download="Oussama_Remli.pdf">My resume</a>
         </div>
         <div className='header__socials'>
           <a href="www.linkedin.com/in/oussamaremli" target="_blank"><BsLinkedin/></a>
           <a href="https://github.com/OussamaRemli" target="_blank"><BsGithub/></a>
         </div>
-        <a href='#contact' className='scroll__down'>Scroll Down</a>
+        <a href='#experience' className='scroll__down'>Scroll Down</a>
 
       </div>
     </header>
